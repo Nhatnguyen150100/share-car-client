de7b0b75fde7ee16a2b35399ee748eca4bb46822
nhatnguyen150100 <nhatnguyen150100@gmail.com>
2023-01-03 10:38:39 +0700

    map fix

--- src/pages/map/Map.jsx
+++ src/pages/map/Map.jsx
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Polyline } from '@react-google-maps/api';
 
 const MapConfig = {
   key: "AIzaSyDCXRPTsCGFIFnVQ-iIGs94mdoD-UN2Kfc"
 }
 
-const containerStyle = {
-  width: '400px',
-  height: '400px'
+const mapContainerStyle = {
+  height: "400px",
+  width: "800px"
 };
 
 const center = {
-  lat: -3.745,
-  lng: -38.523
+  lat: 0,
+  lng: -180
+};
+
+const onLoad = polyline => {
+  console.log('polyline: ', polyline)
+};
+
+const path = [
+  {lat: 37.772, lng: -122.214},
+  {lat: 21.291, lng: -157.821},
+  {lat: -18.142, lng: 178.431},
+  {lat: -27.467, lng: 153.027}
+];
+
+const options = {
+  strokeColor: '#FF0000',
+  strokeOpacity: 0.8,
+  strokeWeight: 2,
+  fillColor: '#FF0000',
+  fillOpacity: 0.35,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  visible: true,
+  radius: 30000,
+  paths: [
+    {lat: 37.772, lng: -122.214},
+    {lat: 21.291, lng: -157.821},
+    {lat: -18.142, lng: 178.431},
+    {lat: -27.467, lng: 153.027}
+  ],
+  zIndex: 1
 };
 
 export default function Map(){
@@ -22,6 +53,11 @@ export default function Map(){
       center={center}
       zoom={10}
     >
+      <Polyline 
+        onLoad={onLoad}
+        path={path}
+        options={options}
+      />
     </GoogleMap>
   </LoadScript>
 }
